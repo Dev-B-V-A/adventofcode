6e4a47a08daf1bc6cf867dab62cb324c24452a6b
vladislav.bobylev <bobylev.v.a@yandex.ru>
2025-12-09 22:07:31 +0300

    FIX: day1: right solve

---
+++ 2025/Day1_Secret_Entrance/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+	int current_pos = 50;
+	int zero_ticks = 0;
+
+	std::ifstream infile ("input.txt");
+
+	std::string current_action;
+	while (infile >> current_action)
+	{
+		if (current_action.empty ())
+			continue;
+
+		int sign = current_action.front() == 'L' ? -1 : 1;
+		int value = std::stoi(current_action.substr(1)) * sign;
+		current_pos = current_pos + value;
+		current_pos = current_pos % 100;
+
+		if (current_pos == 0)
+		    ++zero_ticks;
+	}
+	std::cout << zero_ticks;
+
+	return 0;
+}
